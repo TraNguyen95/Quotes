0af9455ab7fdea74ad76dbf4eec6f050eaf9133f
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-07 03:08:56 +0700

    update root - React 18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import "antd/dist/antd.min.css";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import {store} from "./redux/reducer";
-ReactDOM.render(
+import { store } from "./redux/reducer";
+
+const rootElement = document.getElementById("root");
+
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
   <Provider store={store}>
     <GlobalStyles />
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
