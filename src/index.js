5b56b31208ad3f09ba3406f71031505b72f2d4e4
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-07 07:33:30 +0700

    Remove useless

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import "antd/dist/antd.min.css";
 import { GlobalStyles } from "./styles/GlobalStyles";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/reducer";
 
@@ -17,8 +16,3 @@ root.render(
     <App />
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
